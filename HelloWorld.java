68981a5498383326ece5f0ad8be4dbcd64074213
Nafla <445052094@std.psau.edu.sa>
2026-09-08 13:59:49 +0300

    update code in vs

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main (string[] args){
         System.out.println(Hello, Git!);
-         System.out.println(Hello web);
+         System.out.println(Hello);
     }
 }
